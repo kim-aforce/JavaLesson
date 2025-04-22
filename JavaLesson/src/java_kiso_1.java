8e782796b22f99bf3a3eaf3850d93b7fc746fd38
kim-aforce <j.kim@a-force.co.jp>
2025-04-22 15:29:59 +0900

    問12まで

--- JavaLesson/src/java_kiso_1.java
+++ JavaLesson/src/java_kiso_1.java
@@ -15,17 +15,34 @@ public class java_kiso_1 {
 //	問7.	変数 num に整数 0 を代入し、その値が0かどうかを判定するプログラムを作成してください。									
 		int num7 = 0;
 		if (num7 == 0) {
-			
+			System.out.println("問7：0です。");
+		}else {
+			System.out.println("問7：0ではありません。");
 		}
 //	問8.	変数 num に整数 -5 を代入し、その値が負の数かどうかを判定するプログラムを作成してください。									
-		
+		int num8 = -5;
+		if (num8 < 0) {
+			System.out.println("問8：0より小さいので負の数です。");
+		}
 //	問9.	変数 num に整数 15 を代入し、その値が5の倍数かどうかを判定するプログラムを作成してください。									
-		
+		int num9 = 15;
+		if (num9 % 5 == 0) {
+			System.out.println("問9：15は5の倍数");
+		}else {
+			System.out.println("やり直し");
+		}
 //	問10.	変数 num1 に整数 8 を、num2 に整数 3 を代入し、それらの和を計算して表示するプログラムを作成してください。									
+		int num10 = 8;
+		int num10a = 3;
+		System.out.println("問10：「num10」と「num10a」の和は" + (num10+num10a));
 		
 //	問11.	変数 num1 に整数 10 を、num2 に整数 4 を代入し、それらの差を計算して表示するプログラムを作成してください
-		
+		int num11 = 10;
+		int num11a = 4;
+		System.out.println("問11：「num10」と「num11a」の差は" + (num11 - num11a));
 //	問12.	変数 num1 に整数 6 を、num2 に整数 7 を代入し、それらの積を計算して表示するプログラムを作成してください。									
-
+		int num12 = 6;
+		int num12a = 7;
+		System.out.println("問12：「num12」と「num12a」の積は" + (num12*num12a));
 	}
 }
