ac8e161214e855bae320041f0f71699503d23c9b
kim-aforce <j.kim@a-force.co.jp>
2025-04-30 10:33:31 +0900

    演習問題４ if　完了

--- JavaLesson/src/演習問題/java_kiso_4_if.java
+++ JavaLesson/src/演習問題/java_kiso_4_if.java
@@ -16,7 +16,7 @@ public class java_kiso_4_if {
 		}else {
 			System.out.println("奇数");
 		}
-		
+		//問2
 		Scanner sc2 = new Scanner(System.in);
 		System.out.println("問2：整数を入力");
 		int num2 = sc2.nextInt();	
@@ -26,6 +26,37 @@ public class java_kiso_4_if {
 		}else {
 			System.out.println("正の数");
 		}
+		
+		//問3
+		Scanner sc3 = new Scanner(System.in);
+		System.out.println("1つ目の辺の長さを入力");
+		int tri1 = sc3.nextInt();
+		System.out.println("2つ目の辺の長さを入力");
+		int tri2 = sc3.nextInt();
+		System.out.println("3つ目の辺の長さを入力");
+		int tri3 = sc3.nextInt();
+		
+		 if (tri1 + tri2 > tri3 && tri1 + tri3 > tri2 && tri2 + tri3 > tri1) {
+			 System.out.println("辺の長さは有効");
+		 }else {
+			System.out.println("無効な辺");
+		}
+		
+		//問4
+			Scanner sc4 = new Scanner(System.in);
+			System.out.println("ご年齢を教えてください。");
+			int age = sc4.nextInt();
+		
+			if (age <= 12) {
+				System.out.println("無料");
+			} else if (age > 13 && age <= 59) {
+				System.out.println("1000円です。");
+			}else if (age >= 60) {
+				System.out.println("500円です。");
+			}
+		
+		
+		
 		
 	}
 }
